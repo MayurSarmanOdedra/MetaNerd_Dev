32684aac78743977c495432a2ff3a99a39678077
MayurSarmanOdedra97 <mayursarmanodedra@gmail.com>
2023-08-28 10:59:35 +1200

    second message

---
+++ force-app/main/default/lwc/metaNerd/metaNerd.js
@@ -0,0 +1,107 @@
+import { LightningElement, wire } from 'lwc';
+import getSObjectInfo from '@salesforce/apex/MetaNerdController.getSObjectInfo';
+import getFieldInfo from '@salesforce/apex/MetaNerdController.getFieldInfo';
+import { fieldColumns, selectionOptions } from './util';
+
+
+export default class MetaNerd extends LightningElement {
+    isSObjectSelected = false;
+    options = undefined;
+    recordsData = undefined;
+    selectedSObject;
+    defaultSortDirection = 'asc';
+    sortDirection = 'asc';
+    sortedBy;
+    selectedOptionValues = [];
+
+    @wire(getSObjectInfo)
+    orgSObjects({ error, data}){
+        if(data){
+            this.options = this.sortData([...data]);
+        }else{
+            console.log('No Sobjects returned with error');
+        }
+    }
+
+    //Get SObject Selection Label
+    get sObjectSelectionLabel(){
+        return this.isSObjectSelected ? 'Selected SObject' : 'Select SObject';
+    }
+
+    //Get Data
+    get data(){
+        if(this.isSObjectSelected && this.columns){
+            if(this.selectedOptionValues.includes('customFieldsOnly')){
+                const customFieldsData = this.recordsData.filter((element) => element.isCustom);
+                return customFieldsData;
+            }
+            return [...this.recordsData];
+        }
+    }
+
+    //Get Columns
+    get columns(){
+        if(this.isSObjectSelected){
+            return [...fieldColumns];
+        }
+    }
+
+    //get options
+    get selectOptions() {
+        return [...selectionOptions];
+    }
+
+    handleSObjectComboChange(event){
+        if(event.detail.value){
+            this.isSObjectSelected = true;
+            this.selectedSObject = event.detail.value;
+
+            getFieldInfo({
+                sObjectName: this.selectedSObject
+            }).then((result) => {
+                const receivedFields = [...result];
+                receivedFields.sort(this.sortBy('label', 1));
+                this.recordsData = receivedFields;
+            }).catch((error) => {
+                console.log('Error occured when retrieving SObject fields. ::: ' + JSON.stringify(error));
+            })
+        }
+    }
+
+    onHandleSort(event) {
+        const { fieldName: sortedBy, sortDirection } = event.detail;
+        const cloneData = [...this.recordsData];
+
+        cloneData.sort(this.sortBy(sortedBy, sortDirection === 'asc' ? 1 : -1));
+        this.recordsData = cloneData;
+        this.sortDirection = sortDirection;
+        this.sortedBy = sortedBy;
+    }
+
+    handleSelectOptionChange(event){
+        this.selectedOptionValues = event.detail.value;
+        console.log('current selection values ::: ' + this.selectedOptionValues);
+    }
+
+    sortBy(field, reverse, primer) {
+        const key = primer
+            ? function (x) {
+                  return primer(x[field]);
+              }
+            : function (x) {
+                  return x[field];
+              };
+
+        return function (a, b) {
+            a = key(a);
+            b = key(b);
+            return reverse * ((a > b) - (b > a));
+        };
+    }
+
+    sortData(dataToSort){
+        return dataToSort.sort((a,b) => {
+            return (a.label < b.label ? -1 : (a.label > b.label ? 1 : 0))
+        })
+    }
+}
---
+++ force-app/main/default/lwc/metaNerd/util.js
@@ -0,0 +1,24 @@
+const fieldColumns = [
+    {
+        label: 'Label',
+        fieldName: 'label',
+        sortable: true
+    },
+    {
+        label: 'API Name',
+        fieldName: 'apiName',
+        sortable: true
+    }
+];
+
+const selectionOptions = [
+    {
+        label: 'Show custom fields only',
+        value: 'customFieldsOnly'
+    }
+];
+
+export {
+    fieldColumns,
+    selectionOptions
+};
