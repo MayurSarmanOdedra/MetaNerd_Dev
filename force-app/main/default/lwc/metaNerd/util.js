f9d997bfec474d0a6406ef593339772550249133
MayurSarmanOdedra <mayursarmanodedra@gmail.com>
2024-05-09 21:12:15 +1200

    Revert "MetaNerd to SObjectify segue"
    This reverts commit 49526628672af4f4a027ddbaed9a68cdefe74f0b.

---
+++ force-app/main/default/lwc/metaNerd/util.js
@@ -0,0 +1,28 @@
+const fieldColumns = [
+    {
+        label: 'Label',
+        fieldName: 'label',
+        sortable: true
+    },
+    {
+        label: 'API Name',
+        fieldName: 'apiName',
+        sortable: true
+    }
+];
+
+const selectionOptions = [
+    {
+        label: 'Show custom fields only',
+        value: 'customFieldsOnly'
+    },
+    {
+        label: 'Show unused custom fields only',
+        value: 'unusedCustomFieldsOnly'
+    }
+];
+
+export {
+    fieldColumns,
+    selectionOptions
+};
